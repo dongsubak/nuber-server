42d9debff598a6245481a2b7527ea2d26a3e1246
dongsubak <likeap@gmail.com>
2020-07-03 18:52:25 +0900

    schema allTypes

--- src/schema.ts
+++ src/schema.ts
@@ -1,4 +1,10 @@
 import { GraphQLSchema } from "graphql";
 import { makeExecutableSchema } from "graphql-tools"
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
-// import path from "path";
+import path from "path";
+
+//import all schema from our api
+
+const allTypes : GraphQLSchema[] = fileLoader(
+    path.join(__dirname, "./api/**/*.graphql")
+)
