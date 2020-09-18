6e2893195046ffd72cf6dc65d680f32305a04c1b
dongsubak <likeap@gmail.com>
2020-09-18 19:01:28 +0900

    yarn add finished

--- src/api/Hello/sayHello/sayHello.resolvers.ts
+++ src/api/Hello/sayHello/sayHello.resolvers.ts
@@ -1,17 +1,17 @@
 import { Greeting } from "../../../types/graph";
-//import { SayHelloResponse, slayHelloQueryArgs } from "../../../types/graph"; 
+import { SayHelloResponse, SayHelloQueryArgs } from "../../../types/graph"; 
 const resolvers = {
     Query: {
         // sayHello: () => "Hey hello"
         
-        sayHello: (): Greeting => {
+        sayHelloGreeting: (): Greeting => {
             return {
                 error: false,
                 text: "love you"
             }
-        }
+        },
         
-        /*
+        
         sayHello: (_, args: SayHelloQueryArgs ): SayHelloResponse => {
             console.log(args.name)
             return {
@@ -19,7 +19,7 @@ const resolvers = {
                 text: `Hello ${args.name}`
             }
         }
-        */
+        
     }
 }
 
--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,17 +1,22 @@
-export const typeDefs = ["type Query {\n  sayBye: String!\n  # sayHello(name: String!): SayHelloResponse!\n  sayHello: Greeting!\n}\n\ntype Greeting {\n  text: String!\n  error: Boolean!\n}\n\ntype SayHelloResponse {\n  text: String!\n  error: Boolean!\n}\n"];
+export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello(name: String!): SayHelloResponse!\n  sayHelloGreeting: Greeting!\n}\n\ntype Greeting {\n  text: String!\n  error: Boolean!\n}\n\ntype SayHelloResponse {\n  text: String!\n  error: Boolean!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
   sayBye: string;
-  sayHello: Greeting;
+  sayHello: SayHelloResponse;
+  sayHelloGreeting: Greeting;
 }
 
-export interface Greeting {
+export interface SayHelloQueryArgs {
+  name: string;
+}
+
+export interface SayHelloResponse {
   text: string;
   error: boolean;
 }
 
-export interface SayHelloResponse {
+export interface Greeting {
   text: string;
   error: boolean;
 }
