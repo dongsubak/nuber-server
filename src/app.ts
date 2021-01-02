5f383bd7ccafa05605547d9b7b9a7838a8b189ae
dongsubak <blackxub@naver.com>
2021-01-02 20:36:27 +0900

    start jwtmiddleware

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,25 @@ import logger from "morgan";
 import schema from"./schema"; 
 
 class App {
-    public app: GraphQLServer;
-    constructor(){
-        this.app = new GraphQLServer({
-            schema
-        });
-        this.middlewares();
+  public app: GraphQLServer;
+  constructor() {
+    this.app = new GraphQLServer({
+      schema
+    });
+    this.middlewares();
+  }
+  private middlewares = () : void => {
+    this.app.express.use(cors());
+    this.app.express.use(logger("dev"));
+    this.app.express.use(helmet());
+  };
+  
+  private jwtMiddleware = async(req, res, next): Promise<void> => {
+    const token = req.get("X-JWT");
+    if (token) {
+      
     }
-    private middlewares = () : void => {
-        this.app.express.use(cors());
-        this.app.express.use(logger("dev"));
-        this.app.express.use(helmet());
-    }
+  };
 }
 
 export default new App().app;   
