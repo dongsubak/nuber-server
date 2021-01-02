a9974992482b0b9455108113c16ebdcee5f48498
dongsubak <blackxub@naver.com>
2021-01-02 15:48:53 +0900

    startphoneverification end

--- src/api/User/StartPhoneVerification/StartPhoneVerification.resolvers.ts
+++ src/api/User/StartPhoneVerification/StartPhoneVerification.resolvers.ts
@@ -4,6 +4,7 @@ import {
   StartPhoneVerificationResponse 
 } from "../../../types/graph";
 import Verification from "../../../entities/Verification";
+import { sendVerificationSMS } from "../../../utils/sendSMS";
 
 const resolvers: Resolvers = {
   Mutation: {
@@ -24,6 +25,12 @@ const resolvers: Resolvers = {
           target: "PHONE"
         }).save();
         //newVerification.key
+        console.log(newVerification);
+        await sendVerificationSMS(newVerification.payload, newVerification.key);
+        return {
+          ok: true,
+          error: null
+        }
       } catch (error){
         return {
           ok: false,
