09133eec1b2b0878593be3d04e5391586283de63
dongsubak <likeap@gmail.com>
2021-01-13 13:54:52 +0900

    emailsignup, sendemail

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -4,3 +4,20 @@ const mailGunClient = new Mailgun({
   apiKey: process.env.MAILGUN_API_KEY || '',
   domain: "sandbox5e3256b424ad45069f121c5e45a1d406.mailgun.org"
 });
+
+const sendEmail = (subject: string, html: string, /*to: string*/) => {
+  const emailData = {
+    from: "blackxub@naver.com",
+    to: "blackxub@naver.com",
+    subject,
+    html
+  };
+  return mailGunClient.messages().send(emailData);
+};
+
+export const sendVerificationEmail = (fullName: string, key: string) => {
+  const emailSubject = `Hello! ${fullName}, please verify your email`;
+  const emailBody = `Verify your email by clicking <a href="http://.com/verification/${key}/">here</a>`;
+
+  return sendEmail(emailSubject, emailBody);
+}
